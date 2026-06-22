fede07fa3c608bc6a7dce2f2929e0794e4f7747d
Sean <121244474+Naees@users.noreply.github.com>
2026-06-23 05:38:00 +0800

    fix: add trailing slash to base URL config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  base: '/NaeesWrites',
+  base: '/NaeesWrites/',
   site: 'https://naees.github.io/NaeesWrites',
   integrations: [
     sitemap(),
