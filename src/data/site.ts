c517d536824fb9b2e1fffe8d33047c959457de1b
Sean <121244474+Naees@users.noreply.github.com>
2026-07-06 01:28:25 +0800

    Restyle contact form and update X link

--- src/data/site.ts
+++ src/data/site.ts
@@ -8,7 +8,7 @@ export const site = {
   url: 'https://naees.github.io/NaeesSecurity',
   email: 'naees@protonmail.com',
   github: 'https://github.com/Naees',
-  x: 'https://twitter.com/Naees',
+  x: 'https://x.com/NaeesSecurity',
   signal: 'https://signal.me/#eu/1Fkdlvu4vmAqdT-5QwGdGFH1b4uOrLlpyMlK8Fyr5aw_eu6XMJ1PEYOH18jG1nMY',
   pgpFingerprint: 'EE03 DCB4 E594 D725 C528 83E0 4BCC B80F BFB5 EE54',
   focus: ['web security', 'identity', 'cloud exposure', 'practical exploitation'],
